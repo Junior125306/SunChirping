7787279a5a64a7bc821a71f0affabb6da140735c
张振宇 <000000>
2021-07-22 18:08:42 +0800

    首次提交

---
+++ src/main/java/com/sun/SunSunrise/SunChirpingApplication.java
@@ -0,0 +1,13 @@
+package com.sun.SunSunrise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunChirpingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunChirpingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sun/SunSunrise/SunChirpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.SunSunrise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunChirpingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
